c23a2e01bb037cbe50139e82e1af1d1c6b5fd6b7
sergiomontelobo <sergiomontelobo@4758aa3d-ef4e-0410-909f-09c6bbc04575>
2008-07-16 20:26:15 +0000

    Package renamed

---
+++ src/java/com/google/code/profiling/timer/UtilTimerStack.java
@@ -0,0 +1,80 @@
+package com.google.code.profiling.timer;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * A timer stack.
+ * <p>
+ * Usage:
+ * 
+ * <pre>
+ * String logMessage = &quot;Log message&quot;;
+ * UtilTimerStack.push(logMessage);
+ * try {
+ * 	//do some code
+ * } finally {
+ * 	UtilTimerStack.pop(logMessage); //this needs to be the same text as above
+ * }
+ * </pre>
+ */
+public class UtilTimerStack {
+
+	private static final Log log = LogFactory.getLog(UtilTimerStack.class);
+
+	// A reference to the current ProfilingTimerBean
+	private static final ThreadLocal current = new ThreadLocal();
+
+	private long minTime;
+
+	public UtilTimerStack(long minTime) {
+		this.minTime = minTime;
+	}
+
+	public void push(String name) {
+		// create a new timer and start it
+		ProfilingTimerBean newTimer = new ProfilingTimerBean(name);
+		newTimer.setStartTime();
+
+		// if there is a current timer - add the new timer as a child of it
+		ProfilingTimerBean currentTimer = (ProfilingTimerBean) current.get();
+		if (currentTimer != null) {
+			currentTimer.addChild(newTimer);
+		}
+
+		// set the new timer to be the current timer
+		current.set(newTimer);
+	}
+
+	public void pop(String name) {
+		ProfilingTimerBean currentTimer = (ProfilingTimerBean) current.get();
+
+		// if the timers are matched up with each other (ie push("a");
+		// pop("a"));
+		if ((currentTimer != null) && (name != null)
+				&& name.equals(currentTimer.getResource())) {
+			currentTimer.setEndTime();
+			ProfilingTimerBean parent = currentTimer.getParent();
+			// if we are the root timer, then print out the times
+			if (parent == null) {
+				printTimes(currentTimer);
+				current.set(null); // for those servers that use thread pooling
+			} else {
+				current.set(parent);
+			}
+		} else {
+			// if timers are not matched up, then print what we have, and then
+			// print warning.
+			if (currentTimer != null) {
+				printTimes(currentTimer);
+				current.set(null); // prevent printing multiple times
+				log.warn("Unmatched Timer.  Was expecting "
+						+ currentTimer.getResource() + ", instead got " + name);
+			}
+		}
+	}
+
+	private void printTimes(ProfilingTimerBean currentTimer) {
+		log.info(currentTimer.getPrintable(this.minTime));
+	}
+}
