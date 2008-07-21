082e303625fdc5bc1c5cf0d5415f2f6547ef0872
sergiomontelobo <sergiomontelobo@4758aa3d-ef4e-0410-909f-09c6bbc04575>
2008-07-21 22:12:46 +0000

    Reformating

--- src/java/com/google/code/profiling/timer/ProfilingTimerBean.java
+++ src/java/com/google/code/profiling/timer/ProfilingTimerBean.java
@@ -10,68 +10,70 @@ import java.util.List;
  */
 public class ProfilingTimerBean implements Serializable {
 
-    private static final long serialVersionUID = -678451227367616598L;
+	private static final long serialVersionUID = -678451227367616598L;
 
-    private List children = new ArrayList();
-    private ProfilingTimerBean parent = null;
+	private List children = new ArrayList();
+	private ProfilingTimerBean parent = null;
 
-    private String resource;
+	private String resource;
 
-    private long startTime;
-    private long totalTime;
+	private long startTime;
+	private long totalTime;
 
-    public ProfilingTimerBean(String resource) {
-	this.resource = resource;
-    }
-
-    protected void addParent(ProfilingTimerBean parent) {
-	this.parent = parent;
-    }
-
-    public ProfilingTimerBean getParent() {
-	return this.parent;
-    }
-
-    public void addChild(ProfilingTimerBean child) {
-	this.children.add(child);
-	child.addParent(this);
-    }
-
-    public void setStartTime() {
-	this.startTime = System.currentTimeMillis();
-    }
-
-    public void setEndTime() {
-	this.totalTime = System.currentTimeMillis() - this.startTime;
-    }
-
-    public String getResource() {
-	return this.resource;
-    }
-
-    /**
-     * Get a formatted string representing all the methods that took longer than a specified time.
-     */
-    public String getPrintable(long minTime) {
-	return getPrintable("", minTime);
-    }
-
-    protected String getPrintable(String indent, long minTime) {
-	// only print the value if we are larger or equal to the min time.
-	if (this.totalTime >= minTime) {
-	    StringBuffer buffer = new StringBuffer();
-	    buffer.append(indent);
-	    buffer.append("[" + this.totalTime + "ms] - " + this.resource);
-	    buffer.append("\n");
-
-	    Iterator childrenIt = this.children.iterator();
-	    while (childrenIt.hasNext()) {
-		buffer.append(((ProfilingTimerBean) childrenIt.next()).getPrintable(indent + "  ", minTime));
-	    }
-
-	    return buffer.toString();
-	} else {
-	    return "";
+	public ProfilingTimerBean(String resource) {
+		this.resource = resource;
 	}
-    }
+
+	protected void addParent(ProfilingTimerBean parent) {
+		this.parent = parent;
+	}
+
+	public ProfilingTimerBean getParent() {
+		return this.parent;
+	}
+
+	public void addChild(ProfilingTimerBean child) {
+		this.children.add(child);
+		child.addParent(this);
+	}
+
+	public void setStartTime() {
+		this.startTime = System.currentTimeMillis();
+	}
+
+	public void setEndTime() {
+		this.totalTime = System.currentTimeMillis() - this.startTime;
+	}
+
+	public String getResource() {
+		return this.resource;
+	}
+
+	/**
+	 * Get a formatted string representing all the methods that took longer than
+	 * a specified time.
+	 */
+	public String getPrintable(long minTime) {
+		return getPrintable("", minTime);
+	}
+
+	protected String getPrintable(String indent, long minTime) {
+		// only print the value if we are larger or equal to the min time.
+		if (this.totalTime >= minTime) {
+			StringBuffer buffer = new StringBuffer();
+			buffer.append(indent);
+			buffer.append("[" + this.totalTime + "ms] - " + this.resource);
+			buffer.append("\n");
+
+			Iterator childrenIt = this.children.iterator();
+			while (childrenIt.hasNext()) {
+				buffer.append(((ProfilingTimerBean) childrenIt.next())
+						.getPrintable(indent + "  ", minTime));
+			}
+
+			return buffer.toString();
+		} else {
+			return "";
+		}
+	}
 }
