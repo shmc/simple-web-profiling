a134afda2a61b40708349da5da4642e1d5c31656
sergiomontelobo <sergiomontelobo@4758aa3d-ef4e-0410-909f-09c6bbc04575>
2008-10-03 23:44:54 +0000

    All print (including indent) now uses commons-logging Added unit testing

--- trunk/src/java/com/google/code/profiling/memory/ProfilingMemoryBean.java
+++ trunk/src/java/com/google/code/profiling/memory/ProfilingMemoryBean.java
@@ -5,74 +5,74 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * Bean to contain information about the pages profiled
  */
 public class ProfilingMemoryBean implements Serializable {
 
-	private static final long serialVersionUID = 3467875415909108916L;
+    private static final long serialVersionUID = 3467875415909108916L;
 
-	private List children = new ArrayList();
-	private ProfilingMemoryBean parent = null;
+    private static final Log log = LogFactory.getLog(ProfilingMemoryBean.class);
 
-	private String resource;
+    private List children = new ArrayList();
+    private ProfilingMemoryBean parent = null;
 
-	private long startMemory;
-	private long totalMemory;
+    private String resource;
 
-	public ProfilingMemoryBean(String resource) {
-		this.resource = resource;
+    private long startMemory;
+    private long totalMemory;
+
+    public ProfilingMemoryBean(String resource) {
+	this.resource = resource;
+    }
+
+    protected void addParent(ProfilingMemoryBean parent) {
+	this.parent = parent;
+    }
+
+    public ProfilingMemoryBean getParent() {
+	return this.parent;
+    }
+
+    public void addChild(ProfilingMemoryBean child) {
+	this.children.add(child);
+	child.addParent(this);
+    }
+
+    public void setStartMemory() {
+	this.startMemory = Runtime.getRuntime().freeMemory();
+    }
+
+    public void setEndMemory() {
+	this.totalMemory = Runtime.getRuntime().freeMemory() - this.startMemory;
+    }
+
+    public String getResource() {
+	return this.resource;
+    }
+
+    /**
+     * Get a formatted string representing all the methods that took longer than a specified time.
+     */
+    public void print(long minMemory) {
+	print("", minMemory);
+    }
+
+    protected void print(String indent, long minMemory) {
+	// only print the value if we are larger or equal to the min time.
+	if (this.totalMemory >= minMemory) {
+	    StringBuffer buffer = new StringBuffer();
+	    buffer.append(indent);
+	    buffer.append("[" + this.totalMemory + " KB] - " + this.resource);
+	    log.info(buffer.toString());
+
+	    Iterator childrenIt = this.children.iterator();
+	    while (childrenIt.hasNext()) {
+		((ProfilingMemoryBean) childrenIt.next()).print(indent + "  ", minMemory);
+	    }
 	}
-
-	protected void addParent(ProfilingMemoryBean parent) {
-		this.parent = parent;
-	}
-
-	public ProfilingMemoryBean getParent() {
-		return this.parent;
-	}
-
-	public void addChild(ProfilingMemoryBean child) {
-		this.children.add(child);
-		child.addParent(this);
-	}
-
-	public void setStartMemory() {
-		this.startMemory = Runtime.getRuntime().freeMemory();
-	}
-
-	public void setEndMemory() {
-		this.totalMemory = Runtime.getRuntime().freeMemory() - this.startMemory;
-	}
-
-	public String getResource() {
-		return this.resource;
-	}
-
-	/**
-	 * Get a formatted string representing all the methods that took longer than
-	 * a specified time.
-	 */
-	public String getPrintable(long minMemory) {
-		return getPrintable("", minMemory);
-	}
-
-	protected String getPrintable(String indent, long minMemory) {
-		// only print the value if we are larger or equal to the min time.
-		if (this.totalMemory >= minMemory) {
-			StringBuffer buffer = new StringBuffer();
-			buffer.append(indent);
-			buffer.append("[" + this.totalMemory + "KB] - " + this.resource);
-			buffer.append("\n");
-
-			Iterator childrenIt = this.children.iterator();
-			while (childrenIt.hasNext()) {
-				buffer.append(((ProfilingMemoryBean) childrenIt.next()).getPrintable(indent + "  ", minMemory));
-			}
-
-			return buffer.toString();
-		} else {
-			return "";
-		}
-	}
+    }
 }
